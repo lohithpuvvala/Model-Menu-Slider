abb5c9532e4f7a5048c95627f6ebff03434f5a97
lohithpuvvala <lohithpuvvala@gmail.com>
2025-03-28 22:21:06 +0530

    Updated CSS & JS file. Added functionality for Menu & Modal Toggle

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const close = document.getElementById('close');
 const open = document.getElementById('open');
 const modal = document.getElementById('modal');
 
+//Toggle Nav
 toggle.addEventListener('click', () => {
-    document.body.classList.toggle('show-nav')
+    document.body.classList.toggle('show-nav');
+});
+
+//show modal
+open.addEventListener('click',() => {
+   modal.classList.add('show-modal');
+   document.body.classList.remove('show-nav');
+});
+
+//hide modal
+close.addEventListener('click',() =>{
+    modal.classList.remove('show-modal');
+})
+
+//hide modal on outside click
+window.addEventListener('click', e => {
+    e.target == modal ? modal.classList.remove('show-modal'):false;
 });
